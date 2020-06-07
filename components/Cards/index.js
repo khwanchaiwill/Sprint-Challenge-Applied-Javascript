6b7fdf5bc8855048e4d37037560c769f66c7ea59
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-06-07 02:09:49 -0400

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,59 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+ const cardContainer = document.querySelector('.cards-container')
+function gitHubCard(author){
+
+    // creat the element 
+    const artiCleDiv = document.createElement('div')
+    const headLine = document.createElement('div')
+    const auThorDiv = document.createElement('div')
+    const imgDiv = document.createElement('div')
+    const image = document.createElement('img')
+    const spanTag = document.createElement('span')
+
+    artiCleDiv.classList.add('card')
+    headLine.classList.add('headline')
+    auThorDiv.classList.add('author')
+    imgDiv.classList.add('img-container')
+
+    headLine.textContent = author.headline;
+    image.src = author.authorPhoto
+    spanTag.textContent = author.authorName
+
+
+   artiCleDiv.appendChild(headLine)
+   artiCleDiv.appendChild(auThorDiv)
+   auThorDiv.appendChild(imgDiv)
+   imgDiv.appendChild(image)
+   auThorDiv.appendChild(spanTag)
+
+   return artiCleDiv
+}
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {   
+  
+    console.log('this is good way to try', response.data.articles)
+
+    response.data.articles.bootstrap.forEach((item)=>{
+        cardContainer.appendChild(gitHubCard(item))
+    })
+    response.data.articles.javascript.forEach((item)=>{
+        cardContainer.appendChild(gitHubCard(item))
+    })
+    response.data.articles.jquery.forEach((item)=>{
+        cardContainer.appendChild(gitHubCard(item))
+    })
+    response.data.articles.node.forEach((item)=>{
+        cardContainer.appendChild(gitHubCard(item))
+    })
+    response.data.articles.technology.forEach((item)=>{
+        cardContainer.appendChild(gitHubCard(item))
+    })
+ 
+})
+.catch(error =>{
+    console.log('It is a bad idea', error )
+})
+
--- components/Header/index.js
+++ components/Header/index.js
@@ -10,5 +10,27 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const headerMaker = document.querySelector('.header-container')
+function Header(item) {
+    const headDiv = document.createElement('div')
+    const spanTag = document.createElement('span')
+    const headH1 = document.createElement('h1')
+    const spanTag1 = document.createElement('span')
 
-function Header() {}
+    headDiv.appendChild(spanTag)
+    headDiv.appendChild(headH1)
+    headDiv.appendChild(spanTag1)
+
+
+    headDiv.classList.add('header')
+    spanTag.classList.add('date')
+    spanTag1.classList.add('temp')
+
+    spanTag.textContent = 'SMARCH 28, 2019'
+    headH1.textContent = ' Lambda Times'
+    spanTag1.innerHTML = '98&deg'
+
+return headDiv
+}
+const newHead = Header()
+headerMaker.appendChild(newHead);
